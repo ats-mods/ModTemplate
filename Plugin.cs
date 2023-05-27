91fed7753cec7af8138c8eccfa9f66816aa5ebea
ats-mods <matty_119@hotmail.com>
2023-05-27 14:48:06 +0200

    Generate from template

---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Eremite;
+using Eremite.Controller;
+
+namespace ModTemplate
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance;
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            Instance = this;
+            harmony = Harmony.CreateAndPatchAll(typeof(Plugin));  
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPatch(typeof(MainController), nameof(MainController.OnServicesReady))]
+        [HarmonyPostfix]
+        private static void HookMainControllerSetup()
+        { 
+            // This method will run after game load (Roughly on entering the main menu)
+            // At this point a lot of the game's data will be available.
+            // Your main entry point to access this data will be `Serviceable.Settings` or `MainController.Instance.Settings`
+            Instance.Logger.LogInfo($"Performing game initialization on behalf of {PluginInfo.PLUGIN_GUID}.");
+            Instance.Logger.LogInfo($"The game has loaded {MainController.Instance.Settings.effects.Length} effects.");
+        }
+
+        [HarmonyPatch(typeof(GameController), nameof(GameController.StartGame))]
+        [HarmonyPostfix]
+        private static void HookEveryGameStart()
+        {
+            // Too difficult to predict when GameController will exist and I can hook observers to it
+            // So just use Harmony and save us all some time. This method will run after every game start
+            var isNewGame = MB.GameSaveService.IsNewGame();
+            Instance.Logger.LogInfo($"Entered a game. Is this a new game: {isNewGame}.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Against The Storm Mod Template
+
+This is a basic template for an Against the Storm mod. Because ATS is a unity game, existing mod loaders and techniques can be leveraged to easily create a mod. See the Wiki for a beginner guide on how to do this.
+
+The compiled mod is meant to be loaded into the game with [BepInEx](https://github.com/BepInEx/BepInEx). I tested with the x86 version [5.4.2](https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x86_5.4.21.0.zip). Simply download the loader and unzip it into your game directory. For the Steam version of the game, this will likely be `C:\Program Files (x86)\Steam\steamapps\common\Against the Storm\`. 
+
+The `.dll` file created by this mod (`ModTemplate.dll` by default) can then be dropped into the `BepInEx\plugins` subdirectory inside the game directory. You might have to make this directory yourself, or run the game once to have it made automatically.
+
+This plugin template was initiated from BepInEx plugin template command:
+
+```bash
+dotnet new bepinex5plugin -n ModTemplate -T netstandard2.0 -U 2021.3.15
+```
